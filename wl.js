59e6721358962cea238a7e4a74bfbaf0b95b438f
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-11-24 13:49:11 +0400

    Update wl.js

--- wl.js
+++ wl.js
@@ -38,23 +38,95 @@ client.user.setGame(`Welcoming. `,'https://www.twitch.tv/ImD3s_x');
 }); 
 
 
-client.on('guildMemberAdd', msg => { 
-    var embed = new Discord.RichEmbed()
-    .setAuthor(msg.user.username, msg.user.avatarURL)
-    .setThumbnail(msg.user.avatarURL)
-    .setImage('https://www.google.com.kw/search?biw=1821&bih=876&tbm=isch&sa=1&ei=3P_4W8_PFsOdgAbnmImACw&q=Welcome&oq=Welcome&gs_l=img.3..0i67l2j0l5j0i67j0j0i67.27172.28014..28050...0.0..0.195.195.0j1......1....1..gws-wiz-img.1SdYKhMkc-s#imgrc=Mj2oe2irQFezuM:')     
-    .setTitle('عضو جديد!')
-    .setDescription('مرحبا بك بالسيرفر')
-    .addField('``ايدي العضو``:',"" +  msg.user.id, true)
-    .addField('``تاق العضو``', msg.user.discriminator, true)
-    .addField('``تم الانشاء في``', msg.user.createdAt, true)
-    .addField(' :bust_in_silhouette:  انت رقم',`**[ ${msg.guild.memberCount} ]**`,true)
-    .setColor('GREEN')
-    .setFooter(msg.guild.name, msg.guild.iconURL, true)
-    var channel = msg.guild.channels.find('name', '𝘼𝙂-𝙂𝙚𝙣𝙚𝙧𝙖𝙡')        //تقدر تغير اسم الشانل حق الترحيب
-    if (!channel) return;
-    channel.send({embed : embed});
-    });
+client.on("guildMemberAdd", member => {
+      //const welcomer = client.channels.get("512674548382957573");
+      let welcomer = member.guild.channels.find("name","𝘼𝙂-𝙂𝙚𝙣𝙚𝙧𝙖𝙡 ")
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let heroo = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+         .addField(': تاريخ دخولك السيرفر',`${moment(member.joinedAt).format('D/M/YYYY h:mm a ')} \n\`\`${moment(member.joinedAt).startOf(' ').fromNow()}\`\``, true)      
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:heroo});          
+      
+      const w = ['./img/w1.png',
+      './img/w2.png',
+      './img/w3.png',
+      './img/w4.png',
+      './img/w5.png',
+      './img/w6.png',
+      './img/w7.png',
+      './img/w8.png',
+      './img/w9.png',
+      './img/w10.png',
+      './img/w11.png',
+      './img/w12.png',
+      './img/w13.png',
+      './img/w14.png',
+      './img/w15.png',
+      './img/w16.png',
+      './img/w17.png',
+      './img/w18.png',];
+      
+              let Image = Canvas.Image,
+                  canvas = new Canvas(401, 202),
+                  ctx = canvas.getContext('2d');
+              ctx.patternQuality = 'bilinear';
+              ctx.filter = 'bilinear';
+              ctx.antialias = 'subpixel';
+              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+              ctx.shadowOffsetY = 2;
+              ctx.shadowBlur = 2;
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 401, 202);
+      
+      })
+      
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+      
+                              //AVATARً
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.drawImage(ava, 152, 27, 95, 95);
+                              
+                                                      //wl
+                              ctx.font = '20px Arial Bold';
+                              ctx.fontSize = '20px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+                                                         ctx.fillText(member.user.username, 200, 190);
+                              
+                              //NAMEً
+                              ctx.font = '19px Arial';
+                              ctx.fontSize = '18px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+      ctx.fillText(`Welcome To Arab Gamers ♥ !!`
+                              , 200, 155);
+    welcomer.sendFile(canvas.toBuffer())
+      
+      
+      
+      })
+      })
+      
+      }
+      });
+
 
 
 
