ddec7a703e6c5b357ca3e7d4efc15a0ea4c51818
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-12-07 00:35:52 +0400

    Update me.js

--- me.js
+++ me.js
@@ -44,8 +44,8 @@ client.on('message', message => {
 
   
   
-  client.on('message', omar => {
-var prefix = "!";
+client.on('message', omar => {
+var prefix = "-";
 if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels
 if (!omar.channel.guild) return;
 if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
