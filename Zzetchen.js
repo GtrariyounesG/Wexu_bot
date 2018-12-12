f235b704234d1d7736304f61c1b59e0d06d23e16
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-12-12 12:32:09 +0400

    Update Zzetchen.js

--- Zzetchen.js
+++ Zzetchen.js
@@ -294,8 +294,7 @@ client.on("message", message => {
 
  
 client.on("message", message => {
-	var prefix = "!";
- if (message.content === "g!help") {
+ if (message.content === "!help") {
   const embed = new Discord.RichEmbed()  
       .setColor("#000000") 
       .setDescription(`
