34316db887b45949b0f583c0f6f37273589ea9c6
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-12-15 18:10:29 +0400

    Update cofesexy.js

--- cofesexy.js
+++ cofesexy.js
@@ -45,8 +45,30 @@ client.user.setStatus("dnd");
 
 
 
+client.on('message' , message => {
+    var prefix = "?";
+    let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
+    if(message.content.startsWith(prefix + 'unban')) {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك رتبة`**');
+        if(!user) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
+        message.guild.unban(user);
+        message.guild.owner.send(`لقد تم فك الباند عن الشخص \n ${user} \n By : <@${message.author.id}>`)
+        var embed = new Discord.RichEmbed()
+        .setThumbnail(message.author.avatarURl)
+        .setColor("RANDOM")
+        .setTitle('**●Unban** !')
+        .addField('**●User Unban :** ', `${user}` , true)
+        .addField('**●By :**' ,       ` <@${message.author.id}> ` , true)
+        .setAuthor(message.guild.name)
+        message.channel.sendEmbed(embed)
+    }
+});
+
+
+// BAN --------------------------------------------------------------------------------
+
+var prefix = "$"
 client.on('message', message => {
-    var prefix = "$";
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
  
@@ -62,13 +84,13 @@ client.on('message', message => {
   if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
   let user = message.mentions.users.first();
   let reason = message.content.split(" ").slice(2).join(" ");
-  let b5bzlog = client.channels.find("name", "log");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
  
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a log text channel.");
-  /*if (message.mentions.users.size < 1) return message.channel.send(`.`);
-  if(!reason) return message.channel.send(`https://cdn.pg.sa/fjxlms81nk.png`);*/
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
   if (!message.guild.member(user)
-  .bannable) return message.reply(`This User Is Have High Role !`);
+  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
  
   message.guild.member(user).ban(7, user);
  
@@ -89,44 +111,41 @@ client.on('message', message => {
 
 
 
-
 client.on('message', message => {
-	var prefix = "$"
-  if (message.author.x5bz) return;
+const prefix = "$";
+  if (message.author.kick) return;
   if (!message.content.startsWith(prefix)) return;
-
+ 
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
-
+ 
   let args = message.content.split(" ").slice(1);
-
+ 
   if (command == "kick") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
+               if(!message.channel.guild) return;
          
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
   let user = message.mentions.users.first();
   let reason = message.content.split(" ").slice(2).join(" ");
-  if (message.mentions.users.size < 1) return message.reply("**Use $kick @NAME**");
-  if(!reason) return message.reply ("**..**");
+ 
+  if (message.mentions.users.size < 1) return message.reply("منشن شخص");
+  if(!reason) return message.reply ("اكتب سبب الطرد");
   if (!message.guild.member(user)
-  .kickable) return message.reply("**.**");
-
-  message.guild.member(user).kick();
-
-  const kickembed = new Discord.RichEmbed()
-  .setAuthor(`KICKED!`, user.displayAvatarURL)
+  .bannable) return message.reply("لايمكنني طرد شخص اعلى من رتبتي");
+ 
+  message.guild.member(user).kick(7, user);
+ 
+  const banembed = new Discord.RichEmbed()
+  .setAuthor('Kicked !', user.displayAvatarURL)
   .setColor("RANDOM")
   .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : kickembed
-  })
+  .addField("User:",  `[ + ${user.tag} + ]`)
+  .addField("By:", `[  + ${message.author.tag} +  ]`)
+  .addField("Reason:", `[ + ${reason} +  ]`)
+  client.channels.get("492583022982463500").send({embed : banembed})
 }
 });
-
 
 
 ///----------------------------------------------------------------------------------------------------------
