1a3c2d39104952e914669c1a003f38f5054dbc5e
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-12-18 11:24:59 +0400

    Update cofesexy.js

--- cofesexy.js
+++ cofesexy.js
@@ -520,22 +520,6 @@ client.on('message', function(msg) {
 
 
 
-client.on('message', message => {
-    var args = message.content.split(/[ ]+/)
-    if(message.content.includes('discord.gg')){
-        message.delete()
-      message.channel.sendMessage("", {embed: {
-        title: "لا تنشر",
-        color: 0x06DF00,
-        description: "يمنع النشر في هذا السيرفر",
-        footer: {
-          text: "jumbo"
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
- 
-     
-});
 
 
 
