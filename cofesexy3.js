e90987ac03975963e76afe43b184e466de23f01b
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-12-24 00:03:26 +0400

    Update cofesexy3.js

--- cofesexy3.js
+++ cofesexy3.js
@@ -660,6 +660,37 @@ client.on('message', message => {
     });
 
 
+  client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("526037247879806976");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("526037247879806976");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`**#- Wellllllcome To ~~Coffee Server~~ :coffee: **
+                  **-> User :** ${member} 
+                         **-> Invited BY:** ${Invite.inviter}`) ;         
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
+
+
 
 
 client.on("message", message => {
