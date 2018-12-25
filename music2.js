2a567ab05e9d22ebea8327747addf8341e8ac47d
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2018-12-25 18:23:56 +0400

    Update music2.js

--- music2.js
+++ music2.js
@@ -28,40 +28,29 @@ var prefix = "2";
    
    
    
-const devs = ["399697177259147275"];
-const adminprefix = ["2"];
+ const devs = ['399697177259147275' , '' , ''];
+const adminprefix = "2";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
-
+      
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();
-  } else
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
   if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/imd3s_x");
-      message.channel.send(`**✅**`)
+    client.user.setGame(argresult, "https://www.twitch.tv/ImD3s_x");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});   
+  });
    
    
 const db = require('quick.db');
