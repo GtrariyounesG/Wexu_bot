e85bb16fb5698150c31673fe27ca8d1ff3d569a8
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2019-01-01 05:21:26 +0400

    Update cofesexy3.js

--- cofesexy3.js
+++ cofesexy3.js
@@ -30,7 +30,7 @@ client.on('guildMemberAdd', member => {
 const w = ['./img/welcome_4 - Copy.png'];
  
          let Image = Canvas.Image,
-            canvas = new Canvas(800, 800),
+            canvas = new Canvas(200, 100),
             ctx = canvas.getContext('2d');
         fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
             if (err) return console.log(err);
