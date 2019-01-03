4deba49032a2777c39f93825b6339d144801fd62
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2019-01-03 13:29:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -562,10 +562,35 @@ if(message.content.startsWith( '?' + 'bc')) {
 
 
 
+client.on("message", message => {
+    var prefix = "?";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "clear")) {
+				if(!message.channel.guild) return;
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**No Permissions**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done",
+        color: 0x06DF00,
+        description: "Chat Deleted",
+        footer: {
+          text: "Canon Bot©."
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+
+
+
 
 
 
 ////set
 
 client.login(process.env.BOT_TOKEN);
-
