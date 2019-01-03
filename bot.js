38e26e82ca09f469b776ed92226d8e792f3785f1
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2019-01-04 02:26:46 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -605,13 +605,14 @@ if(!message.channel.guild) return
 if(message.content.startsWith( '?' + 'bc')) {
         message.guild.members.forEach(member => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            member.send(`** ${args}**		
- ${member} `);
+            member.send(`${args}\n ${member}`);
 
         });
     }
+	
 
 });
+
 
 
 /// clear 
