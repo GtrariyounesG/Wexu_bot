daa6813a070557294454f6019afc8f528e2c38a8
ImZa1D <43499446+ImZa1D@users.noreply.github.com>
2019-01-04 04:41:28 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,7 +158,7 @@ client.on('guildMemberAdd',async member => {
     if(welcome[member.guild.id].onoff === 'Off') return;
     const Canvas = require('canvas');
     const jimp = require('jimp');
-    const w = ['w1.png'];
+    const w = ['welcome_4.png'];
           let Image = Canvas.Image,
               canvas = new Canvas(800, 300),
               ctx = canvas.getContext('2d');
